08545b471664d2382731a23b6f002f472e93a9be
Maxim Anfinogenov <m.anfinogenov0@gmail.com>
2017-10-31 13:16:28 +0300

    Added 'exec' option to compilation script
    Signed-off-by: Maxim Anfinogenov <m.anfinogenov0@gmail.com>

--- compile.sh
+++ compile.sh
@@ -5,6 +5,9 @@ echo -n "Compilation start"
 time gcc `ls . | grep .c$` -o ../main.out &&
 echo -e "Compilation success!\n" 
 cd .. 
-echo "Execution start"
-./main.out
+if [[ $1 == 'exec' ]] 
+then
+    echo "Execution start"
+    ./main.out
+fi
 
